moby/moby

Add builtin nodes discovery
Use `pkg/discovery` to provide nodes discovery between daemon instances.

The functionality is driven by two different command-line flags:
`--discovery-backend` and `--discovery-address`. It can be used in two
ways by interested components:

1. Externally by calling the `/info` API and examining the discovery
   backend. The `pkg/discovery` package can then be used to hit the same
   endpoint and watch for appearing or disappearing nodes. That is the
   method that will for example be used by Swarm.
2. Internally by using the `Daemon.discoveryWatcher` instance. That is
   the method that will for example be used by libnetwork.

Signed-off-by: Arnaud Porterie <arnaud.porterie@docker.com>

api/types/types.go
@@ -215,6 +215,7 @@ type Info struct {
 	Name               string
 	Labels             []string
 	ExperimentalBuild  bool
+	DiscoveryBackend   string
 }
 
 // ExecStartCheck is a temp struct used by execStart

integration-cli/docker_cli_info_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
+	"github.com/docker/docker/pkg/integration/checker"
 	"github.com/docker/docker/utils"
 	"github.com/go-check/check"
 )
@@ -34,4 +36,20 @@ func (s *DockerSuite) TestInfoEnsureSucceeds(c *check.C) {
 			c.Errorf("couldn't find string %v in output", linePrefix)
 		}
 	}
+}
+
+// TestInfoDiscoveryBackend verifies that a daemon run with `--discovery-address` and
+// `--discovery-backend` properly show the backend's endpoint in info output.
+func (s *DockerSuite) TestInfoDiscoveryBackend(c *check.C) {
+	discoveryBackend := "consul://consuladdr:consulport/some/path"
+
+	d := NewDaemon(c)
+	if err := d.Start(fmt.Sprintf("--discovery-backend=%s", discoveryBackend), "--discovery-address=foo"); err != nil {
+		c.Fatal(err)
+	}
+	defer d.Stop()
+
+	out, err := d.Cmd("info")
+	c.Assert(err, checker.IsNil)
+	c.Assert(out, checker.Contains, fmt.Sprintf("Discovery backend: %s\n", discoveryBackend))
 }

docker-archive/docker-ce

The functionality is driven by two different command-line flags: the
experimental `--cluster-store` (previously `--kv-store`) and
`--cluster-advertise`. It can be used in two ways by interested
components:
1. Externally by calling the `/info` API and examining the cluster store
   field. The `pkg/discovery` package can then be used to hit the same
Upstream-commit: 7d193ef1f3b5fcd6aa55b7376116e2617be12e06
Component: engine

components/engine/api/types/types.go
@@ -216,6 +216,7 @@ type Info struct {
 	Labels             []string
 	ExperimentalBuild  bool
 	ServerVersion      string
+	ClusterStore       string
 }
 
 // ExecStartCheck is a temp struct used by execStart

components/engine/integration-cli/docker_cli_info_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
+	"github.com/docker/docker/pkg/integration/checker"
 	"github.com/docker/docker/utils"
 	"github.com/go-check/check"
 )
@@ -34,4 +36,21 @@ func (s *DockerSuite) TestInfoEnsureSucceeds(c *check.C) {
 			c.Errorf("couldn't find string %v in output", linePrefix)
 		}
 	}
+}
+
+// TestInfoDiscoveryBackend verifies that a daemon run with `--cluster-advertise` and
+// `--cluster-store` properly show the backend's endpoint in info output.
+func (s *DockerSuite) TestInfoDiscoveryBackend(c *check.C) {
+	testRequires(c, SameHostDaemon)
+
+	d := NewDaemon(c)
+	discoveryBackend := "consul://consuladdr:consulport/some/path"
+	if err := d.Start(fmt.Sprintf("--cluster-store=%s", discoveryBackend), "--cluster-advertise=foo"); err != nil {
+		c.Fatal(err)
+	}
+	defer d.Stop()
+
+	out, err := d.Cmd("info")
+	c.Assert(err, checker.IsNil)
+	c.Assert(out, checker.Contains, fmt.Sprintf("Cluster store: %s\n", discoveryBackend))
 }