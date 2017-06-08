ba21fcb357cee3dd6c908bb4095e500e3010088d
Vincent Demeester <vincent@sbr.pm>
2017-06-08 10:44:05 +0200

    Update ConvertService for external usage 👼
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>
    Upstream-commit: 96dc07a8cf845c56d2756dabfded2637e1c6e70d
    Component: cli

--- components/cli/cli/compose/convert/service.go
+++ components/cli/cli/compose/convert/service.go
@@ -41,7 +41,7 @@ func Services(
 			return nil, errors.Wrapf(err, "service %s", service.Name)
 		}
 
-		serviceSpec, err := convertService(client.ClientVersion(), namespace, service, networks, volumes, secrets, configs)
+		serviceSpec, err := Service(client.ClientVersion(), namespace, service, networks, volumes, secrets, configs)
 		if err != nil {
 			return nil, errors.Wrapf(err, "service %s", service.Name)
 		}
@@ -51,7 +51,8 @@ func Services(
 	return result, nil
 }
 
-func convertService(
+// Service converts a ServiceConfig into a swarm ServiceSpec
+func Service(
 	apiVersion string,
 	namespace Namespace,
 	service composetypes.ServiceConfig,
