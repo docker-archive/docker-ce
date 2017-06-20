ycllz/docker

LCOW: Pass platform through into layer store
Signed-off-by: John Howard <jhoward@microsoft.com>

daemon/commit.go
@@ -163,6 +163,7 @@ func (daemon *Daemon) Commit(name string, c *backend.ContainerCommitConfig) (str
 	var history []image.History
 	rootFS := image.NewRootFS()
 	osVersion := ""
+	os := ""
 	var osFeatures []string
 
 	if container.ImageID != "" {
@@ -174,9 +175,13 @@ func (daemon *Daemon) Commit(name string, c *backend.ContainerCommitConfig) (str
 		rootFS = img.RootFS
 		osVersion = img.OSVersion
 		osFeatures = img.OSFeatures
+		// To support LCOW, Windows needs to pass the platform in when registering the layer in the store
+		if runtime.GOOS == "windows" {
+			os = img.OS
+		}
 	}
 
-	l, err := daemon.layerStore.Register(rwTar, rootFS.ChainID())
+	l, err := daemon.layerStore.Register(rwTar, rootFS.ChainID(), layer.Platform(os))
 	if err != nil {
 		return "", err
 	}

docker-archive/docker-ce

Upstream-commit: 42c5c1a9ec14f00d5a5367131493cbd6de7d72b0
Component: engine

components/engine/daemon/commit.go
@@ -160,6 +160,7 @@ func (daemon *Daemon) Commit(name string, c *backend.ContainerCommitConfig) (str
 	}()
 
 	var parent *image.Image
+	os := runtime.GOOS
 	if container.ImageID == "" {
 		parent = new(image.Image)
 		parent.RootFS = image.NewRootFS()
@@ -168,9 +169,13 @@ func (daemon *Daemon) Commit(name string, c *backend.ContainerCommitConfig) (str
 		if err != nil {
 			return "", err
 		}
+		// To support LCOW, Windows needs to pass the platform in when registering the layer in the store
+		if runtime.GOOS == "windows" {
+			os = parent.OS
+		}
 	}
 
-	l, err := daemon.layerStore.Register(rwTar, parent.RootFS.ChainID())
+	l, err := daemon.layerStore.Register(rwTar, parent.RootFS.ChainID(), layer.Platform(os))
 	if err != nil {
 		return "", err
 	}