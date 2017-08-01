6eb6b7d9d257e591986c69a2334b8a9ac84ca007
khaled souf <khaled.souf@gmail.com>
2017-08-01 11:06:47 +0200

    adding network list test
    Signed-off-by: khaled souf <khaled.souf@gmail.com>
    Upstream-commit: 15d92a9e0311cd0aa2acccad77eeb7759f46ffe6
    Component: cli

--- components/cli/cli/command/network/client_test.go
+++ components/cli/cli/command/network/client_test.go
@@ -12,6 +12,7 @@ type fakeClient struct {
 	networkCreateFunc     func(ctx context.Context, name string, options types.NetworkCreate) (types.NetworkCreateResponse, error)
 	networkConnectFunc    func(ctx context.Context, networkID, container string, config *network.EndpointSettings) error
 	networkDisconnectFunc func(ctx context.Context, networkID, container string, force bool) error
+	networkListFunc       func(ctx context.Context, options types.NetworkListOptions) ([]types.NetworkResource, error)
 }
 
 func (c *fakeClient) NetworkCreate(ctx context.Context, name string, options types.NetworkCreate) (types.NetworkCreateResponse, error) {
@@ -33,4 +34,11 @@ func (c *fakeClient) NetworkDisconnect(ctx context.Context, networkID, container
 		return c.networkDisconnectFunc(ctx, networkID, container, force)
 	}
 	return nil
+}
+
+func (c *fakeClient) NetworkList(ctx context.Context, options types.NetworkListOptions) ([]types.NetworkResource, error) {
+	if c.networkListFunc != nil {
+		return c.networkListFunc(ctx, options)
+	}
+	return []types.NetworkResource{}, nil
 }
