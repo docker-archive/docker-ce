moby/moby

Added tag log option to json-logger
Fixes #19803
Updated the json-logger to utilize the common log option
'tag' that can define container/image information to include
as part of logging.

When the 'tag' log option is not included, there is no change
to the log content via the json-logger. When the 'tag' log option
is included, the tag will be parsed as a template and the result
will be stored within each log entry as the attribute 'tag'.

Update: Removing test added to integration_cli as those have been deprecated.
Update: Using proper test calls (require and assert) in jsonfilelog_test.go based on review.
Update: Added new unit test configs for logs with tag. Updated unit test error checking.
Update: Cleanup check in jsonlogbytes_test.go to match pending changes in PR #34946.
Update: Merging to correct conflicts from PR #34946.

Signed-off-by: bonczj <josh.bonczkowski@gmail.com>

daemon/logger/jsonfilelog/jsonfilelog.go
@@ -31,6 +31,7 @@ type JSONFileLogger struct {
 	closed  bool
 	writer  *loggerutils.RotateFileWriter
 	readers map[*logger.LogWatcher]struct{} // stores the active log followers
+	tag     string                          // tag values requested by the user to log
 }
 
 func init() {
@@ -65,6 +66,12 @@ func New(info logger.Info) (logger.Logger, error) {
 		}
 	}
 
+	// no default template. only use a tag if the user asked for it
+	tag, err := loggerutils.ParseLogTag(info, "")
+	if err != nil {
+		return nil, err
+	}
+
 	writer, err := loggerutils.NewRotateFileWriter(info.LogPath, capval, maxFiles)
 	if err != nil {
 		return nil, err
@@ -88,20 +95,21 @@ func New(info logger.Info) (logger.Logger, error) {
 		writer:  writer,
 		readers: make(map[*logger.LogWatcher]struct{}),
 		extra:   extra,
+		tag:     tag,
 	}, nil
 }
 
 // Log converts logger.Message to jsonlog.JSONLog and serializes it to file.
 func (l *JSONFileLogger) Log(msg *logger.Message) error {
 	l.mu.Lock()
-	err := writeMessageBuf(l.writer, msg, l.extra, l.buf)
+	err := writeMessageBuf(l.writer, msg, l.extra, l.buf, l.tag)
 	l.buf.Reset()
 	l.mu.Unlock()
 	return err
 }
 
-func writeMessageBuf(w io.Writer, m *logger.Message, extra json.RawMessage, buf *bytes.Buffer) error {
-	if err := marshalMessage(m, extra, buf); err != nil {
+func writeMessageBuf(w io.Writer, m *logger.Message, extra json.RawMessage, buf *bytes.Buffer, tag string) error {
+	if err := marshalMessage(m, extra, buf, tag); err != nil {
 		logger.PutMessage(m)
 		return err
 	}
@@ -110,7 +118,7 @@ func writeMessageBuf(w io.Writer, m *logger.Message, extra json.RawMessage, buf 
 	return errors.Wrap(err, "error writing log entry")
 }
 
-func marshalMessage(msg *logger.Message, extra json.RawMessage, buf *bytes.Buffer) error {
+func marshalMessage(msg *logger.Message, extra json.RawMessage, buf *bytes.Buffer, tag string) error {
 	logLine := msg.Line
 	if !msg.Partial {
 		logLine = append(msg.Line, '\n')
@@ -120,6 +128,7 @@ func marshalMessage(msg *logger.Message, extra json.RawMessage, buf *bytes.Buffe
 		Stream:   msg.Source,
 		Created:  msg.Timestamp,
 		RawAttrs: extra,
+		Tag:      tag,
 	}).MarshalJSONBuf(buf)
 	if err != nil {
 		return errors.Wrap(err, "error writing log message to buffer")
@@ -137,6 +146,7 @@ func ValidateLogOpt(cfg map[string]string) error {
 		case "labels":
 		case "env":
 		case "env-regex":
+		case "tag":
 		default:
 			return fmt.Errorf("unknown log opt '%s' for json-file log driver", key)
 		}

docker-archive/docker-ce

Upstream-commit: 5f50f4f511cd84e79bf005817af346b1764df27f
Component: engine

components/engine/daemon/logger/jsonfilelog/jsonfilelog.go
@@ -27,6 +27,7 @@ type JSONFileLogger struct {
 	closed  bool
 	writer  *loggerutils.LogFile
 	readers map[*logger.LogWatcher]struct{} // stores the active log followers
+	tag     string                          // tag values requested by the user to log
 }
 
 func init() {
@@ -61,6 +62,12 @@ func New(info logger.Info) (logger.Logger, error) {
 		}
 	}
 
+	// no default template. only use a tag if the user asked for it
+	tag, err := loggerutils.ParseLogTag(info, "")
+	if err != nil {
+		return nil, err
+	}
+
 	var extra []byte
 	attrs, err := info.ExtraAttributes(nil)
 	if err != nil {
@@ -76,7 +83,7 @@ func New(info logger.Info) (logger.Logger, error) {
 
 	buf := bytes.NewBuffer(nil)
 	marshalFunc := func(msg *logger.Message) ([]byte, error) {
-		if err := marshalMessage(msg, extra, buf); err != nil {
+		if err := marshalMessage(msg, extra, buf, tag); err != nil {
 			return nil, err
 		}
 		b := buf.Bytes()
@@ -92,6 +99,7 @@ func New(info logger.Info) (logger.Logger, error) {
 	return &JSONFileLogger{
 		writer:  writer,
 		readers: make(map[*logger.LogWatcher]struct{}),
+		tag:     tag,
 	}, nil
 }
 
@@ -103,7 +111,7 @@ func (l *JSONFileLogger) Log(msg *logger.Message) error {
 	return err
 }
 
-func marshalMessage(msg *logger.Message, extra json.RawMessage, buf *bytes.Buffer) error {
+func marshalMessage(msg *logger.Message, extra json.RawMessage, buf *bytes.Buffer, tag string) error {
 	logLine := msg.Line
 	if !msg.Partial {
 		logLine = append(msg.Line, '\n')
@@ -113,6 +121,7 @@ func marshalMessage(msg *logger.Message, extra json.RawMessage, buf *bytes.Buffe
 		Stream:   msg.Source,
 		Created:  msg.Timestamp,
 		RawAttrs: extra,
+		Tag:      tag,
 	}).MarshalJSONBuf(buf)
 	if err != nil {
 		return errors.Wrap(err, "error writing log message to buffer")
@@ -130,6 +139,7 @@ func ValidateLogOpt(cfg map[string]string) error {
 		case "labels":
 		case "env":
 		case "env-regex":
+		case "tag":
 		default:
 			return fmt.Errorf("unknown log opt '%s' for json-file log driver", key)
 		}