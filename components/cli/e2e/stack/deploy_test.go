f1846c5ab0b71d9b428403a886f10844e21503d6
Daniel Nephin <dnephin@docker.com>
2018-03-12 15:02:18 -0400

    Add e2e test for stack deploy
    Signed-off-by: Daniel Nephin <dnephin@docker.com>
    Upstream-commit: fcffd6702811a59c43d6fd063f63c008c5ce8ddd
    Component: cli

---
+++ components/cli/e2e/stack/deploy_test.go
@@ -0,0 +1,26 @@
+package stack
+
+import (
+	"sort"
+	"strings"
+	"testing"
+
+	"github.com/gotestyourself/gotestyourself/assert"
+	"github.com/gotestyourself/gotestyourself/golden"
+	"github.com/gotestyourself/gotestyourself/icmd"
+)
+
+func TestDeployWithNamedResources(t *testing.T) {
+	stackname := "test-stack-deploy-with-names"
+	composefile := golden.Path("stack-with-named-resources.yml")
+
+	result := icmd.RunCommand(
+		"docker", "stack", "deploy", "-c", composefile, stackname)
+
+	result.Assert(t, icmd.Success)
+	stdout := strings.Split(result.Stdout(), "\n")
+	expected := strings.Split(string(golden.Get(t, "stack-deploy-with-nanes.golden")), "\n")
+	sort.Strings(stdout)
+	sort.Strings(expected)
+	assert.DeepEqual(t, stdout, expected)
+}
