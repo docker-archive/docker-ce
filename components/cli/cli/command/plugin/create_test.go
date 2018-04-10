docker/cli

Fixes some unit tests to be able to run them on windows
Some of them are skipped for now (because the feature is not supported
or needs more work), some of them are fixed.

Signed-off-by: Vincent Demeester <vincent@sbr.pm>

cli/command/plugin/create_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"runtime"
 	"testing"
 
 	"github.com/docker/cli/internal/test"
@@ -14,7 +15,10 @@ import (
 )
 
 func TestCreateErrors(t *testing.T) {
-
+	noSuchFile := "no such file or directory"
+	if runtime.GOOS == "windows" {
+		noSuchFile = "The system cannot find the file specified."
+	}
 	testCases := []struct {
 		args          []string
 		expectedError string
@@ -29,7 +33,7 @@ func TestCreateErrors(t *testing.T) {
 		},
 		{
 			args:          []string{"plugin-foo", "nonexistent_context_dir"},
-			expectedError: "no such file or directory",
+			expectedError: noSuchFile,
 		},
 	}
 
@@ -61,7 +65,11 @@ func TestCreateErrorOnContextDirWithoutConfig(t *testing.T) {
 	cmd := newCreateCommand(cli)
 	cmd.SetArgs([]string{"plugin-foo", tmpDir.Path()})
 	cmd.SetOutput(ioutil.Discard)
-	testutil.ErrorContains(t, cmd.Execute(), "config.json: no such file or directory")
+	expectedErr := "config.json: no such file or directory"
+	if runtime.GOOS == "windows" {
+		expectedErr = "config.json: The system cannot find the file specified."
+	}
+	testutil.ErrorContains(t, cmd.Execute(), expectedErr)
 }
 
 func TestCreateErrorOnInvalidConfig(t *testing.T) {

cli/command/trust/signer_add_test.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/docker/cli/cli/config"
@@ -96,7 +97,11 @@ func TestSignerAddCommandBadKeyPath(t *testing.T) {
 	cmd.SetArgs([]string{"--key", "/path/to/key.pem", "alice", "alpine"})
 
 	cmd.SetOutput(ioutil.Discard)
-	assert.EqualError(t, cmd.Execute(), "unable to read public key from file: open /path/to/key.pem: no such file or directory")
+	expectedError := "unable to read public key from file: open /path/to/key.pem: no such file or directory"
+	if runtime.GOOS == "windows" {
+		expectedError = "unable to read public key from file: open /path/to/key.pem: The system cannot find the path specified."
+	}
+	assert.EqualError(t, cmd.Execute(), expectedError)
 }
 
 func TestSignerAddCommandInvalidRepoName(t *testing.T) {
@@ -127,7 +132,11 @@ func TestSignerAddCommandInvalidRepoName(t *testing.T) {
 func TestIngestPublicKeys(t *testing.T) {
 	// Call with a bad path
 	_, err := ingestPublicKeys([]string{"foo", "bar"})
-	assert.EqualError(t, err, "unable to read public key from file: open foo: no such file or directory")
+	expectedError := "unable to read public key from file: open foo: no such file or directory"
+	if runtime.GOOS == "windows" {
+		expectedError = "unable to read public key from file: open foo: The system cannot find the file specified."
+	}
+	assert.EqualError(t, err, expectedError)
 	// Call with real file path
 	tmpfile, err := ioutil.TempFile("", "pemfile")
 	assert.NoError(t, err)

docker-archive/docker-ce

(cherry picked from commit 0cf2e6353a88a12b78da72db6a7e7c7d049d3ed8)
Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

components/cli/cli/command/plugin/create_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"runtime"
 	"testing"
 
 	"github.com/docker/cli/internal/test"
@@ -14,7 +15,10 @@ import (
 )
 
 func TestCreateErrors(t *testing.T) {
-
+	noSuchFile := "no such file or directory"
+	if runtime.GOOS == "windows" {
+		noSuchFile = "The system cannot find the file specified."
+	}
 	testCases := []struct {
 		args          []string
 		expectedError string
@@ -29,7 +33,7 @@ func TestCreateErrors(t *testing.T) {
 		},
 		{
 			args:          []string{"plugin-foo", "nonexistent_context_dir"},
-			expectedError: "no such file or directory",
+			expectedError: noSuchFile,
 		},
 	}
 
@@ -61,7 +65,12 @@ func TestCreateErrorOnContextDirWithoutConfig(t *testing.T) {
 	cmd := newCreateCommand(cli)
 	cmd.SetArgs([]string{"plugin-foo", tmpDir.Path()})
 	cmd.SetOutput(ioutil.Discard)
-	assert.ErrorContains(t, cmd.Execute(), "config.json: no such file or directory")
+
+	expectedErr := "config.json: no such file or directory"
+	if runtime.GOOS == "windows" {
+		expectedErr = "config.json: The system cannot find the file specified."
+	}
+	assert.ErrorContains(t, cmd.Execute(), expectedErr)
 }
 
 func TestCreateErrorOnInvalidConfig(t *testing.T) {

components/cli/cli/command/trust/signer_add_test.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/docker/cli/cli/config"
@@ -97,7 +98,11 @@ func TestSignerAddCommandBadKeyPath(t *testing.T) {
 	cmd.SetArgs([]string{"--key", "/path/to/key.pem", "alice", "alpine"})
 
 	cmd.SetOutput(ioutil.Discard)
-	assert.Error(t, cmd.Execute(), "unable to read public key from file: open /path/to/key.pem: no such file or directory")
+	expectedError := "unable to read public key from file: open /path/to/key.pem: no such file or directory"
+	if runtime.GOOS == "windows" {
+		expectedError = "unable to read public key from file: open /path/to/key.pem: The system cannot find the path specified."
+	}
+	assert.Error(t, cmd.Execute(), expectedError)
 }
 
 func TestSignerAddCommandInvalidRepoName(t *testing.T) {
@@ -128,7 +133,11 @@ func TestSignerAddCommandInvalidRepoName(t *testing.T) {
 func TestIngestPublicKeys(t *testing.T) {
 	// Call with a bad path
 	_, err := ingestPublicKeys([]string{"foo", "bar"})
-	assert.Error(t, err, "unable to read public key from file: open foo: no such file or directory")
+	expectedError := "unable to read public key from file: open foo: no such file or directory"
+	if runtime.GOOS == "windows" {
+		expectedError = "unable to read public key from file: open foo: The system cannot find the file specified."
+	}
+	assert.Error(t, err, expectedError)
 	// Call with real file path
 	tmpfile, err := ioutil.TempFile("", "pemfile")
 	assert.NilError(t, err)