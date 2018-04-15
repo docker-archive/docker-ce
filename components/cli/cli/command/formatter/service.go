8531403103b36f9a02d1076ffbbb2e255b3178b6
Essam A. Hassan <es.hassan187@gmail.com>
2018-04-15 19:38:59 +0200

    adding configs/secrets to service inspect pretty
    Signed-off-by: Essam A. Hassan <es.hassan187@gmail.com>
    Upstream-commit: 3ae2d4b24c2156240503f47f42c158318a5df88a
    Component: cli

--- components/cli/cli/command/formatter/service.go
+++ components/cli/cli/command/formatter/service.go
@@ -97,6 +97,18 @@ Mounts:
    ReadOnly = {{ $mount.ReadOnly }}
    Type = {{ $mount.Type }}
 {{- end -}}
+{{- if .Configs}}
+Configs:
+{{- range $config := .Configs }}
+ Target:	{{$config.File.Name}}
+  Source:	{{$config.ConfigName}}
+{{- end }}{{ end }}
+{{- if .Secrets }}
+Secrets:
+{{- range $secret := .Secrets }}
+ Target:	{{$secret.File.Name}}
+  Source:	{{$secret.SecretName}}
+{{- end }}{{ end }}
 {{- if .HasResources }}
 Resources:
 {{- if .HasResourceReservations }}
@@ -198,6 +210,14 @@ func (ctx *serviceInspectContext) Name() string {
 
 func (ctx *serviceInspectContext) Labels() map[string]string {
 	return ctx.Service.Spec.Labels
+}
+
+func (ctx *serviceInspectContext) Configs() []*swarm.ConfigReference {
+	return ctx.Service.Spec.TaskTemplate.ContainerSpec.Configs
+}
+
+func (ctx *serviceInspectContext) Secrets() []*swarm.SecretReference {
+	return ctx.Service.Spec.TaskTemplate.ContainerSpec.Secrets
 }
 
 func (ctx *serviceInspectContext) IsModeGlobal() bool {
