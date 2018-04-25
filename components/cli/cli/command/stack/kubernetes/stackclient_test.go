57c3c8945501068545120d218448436cb34a106c
Simon Ferquel <simon.ferquel@docker.com>
2018-04-25 15:43:18 +0200

    Make sure every composefiles sent to a v1beta1 endpoint are parsable
    Signed-off-by: Simon Ferquel <simon.ferquel@docker.com>
    Upstream-commit: f766aff71603db951414d4d1bd36ce44c0bd080b
    Component: cli

--- components/cli/cli/command/stack/kubernetes/stackclient_test.go
+++ components/cli/cli/command/stack/kubernetes/stackclient_test.go
@@ -26,7 +26,7 @@ func TestFromCompose(t *testing.T) {
 	})
 	assert.NilError(t, err)
 	assert.Equal(t, "foo", s.name)
-	assert.Equal(t, string(`version: "3.1"
+	assert.Equal(t, string(`version: "3.5"
 services:
   bar:
     image: bar
@@ -37,4 +37,28 @@ volumes: {}
 secrets: {}
 configs: {}
 `), s.composeFile)
+}
+
+func TestFromComposeUnsupportedVersion(t *testing.T) {
+	stackClient := &stackV1Beta1{}
+	_, err := stackClient.FromCompose(ioutil.Discard, "foo", &composetypes.Config{
+		Version:  "3.6",
+		Filename: "banana",
+		Services: []composetypes.ServiceConfig{
+			{
+				Name:  "foo",
+				Image: "foo",
+				Volumes: []composetypes.ServiceVolumeConfig{
+					{
+						Type:   "tmpfs",
+						Target: "/app",
+						Tmpfs: &composetypes.ServiceVolumeTmpfs{
+							Size: 10000,
+						},
+					},
+				},
+			},
+		},
+	})
+	assert.ErrorContains(t, err, "the compose yaml file is invalid with v3.5: services.foo.volumes.0 Additional property tmpfs is not allowed")
 }
