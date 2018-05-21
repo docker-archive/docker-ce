c83bf05cc2b2151e9a376b285bee588f7fa4e77a
Anda Xu <anda.xu@docker.com>
2018-05-21 15:57:08 -0700

    move inspect test from moby to cli
    Signed-off-by: Anda Xu <anda.xu@docker.com>
    Upstream-commit: 2d8d5fcd30ab99002d75853ae6f803cfb445aa7a
    Component: cli

---
+++ components/cli/e2e/system/inspect_test.go
@@ -0,0 +1,16 @@
+package system
+
+import (
+	"testing"
+
+	"github.com/gotestyourself/gotestyourself/icmd"
+)
+
+// TestInspectInvalidReference migrated from moby/integration-cli
+func TestInspectInvalidReference(t *testing.T) {
+	// This test should work on both Windows and Linux
+	result := icmd.RunCmd(icmd.Command("docker", "inspect", "FooBar"))
+	result.Assert(t, icmd.Expected{
+		Out: "[]\nError: No such object: FooBar",
+	})
+}
