5fdb130025e4fb351d0f6900475a4bf20f8e1271
Andrew Hsu <andrewhsu@docker.com>
2018-08-23 06:08:57 +0000

    use systemctl is-active to check for containerd
    The other way was not working.
    
    Signed-off-by: Andrew Hsu <andrewhsu@docker.com>
    Upstream-commit: f00df1bf1efa2ed1ea508ea7b1c291500441571d
    Component: packaging

--- components/packaging/containerd.mk
+++ components/packaging/containerd.mk
@@ -4,7 +4,7 @@ CONTAINERD_PROXY_COMMIT=82ae3d13e91d062dd4853379fe018638023c8da2
 CONTAINERD_SHIM_PROCESS_IMAGE=docker.io/docker/containerd-shim-process:ff98a47
 
 # If containerd is running use that socket instead
-ifeq ($(shell systemctl status containerd 2>/dev/null >/dev/null && echo -n "yes"), "yes")
+ifeq ("$(shell systemctl is-active containerd)", "active")
 CONTAINERD_SOCK:=/var/run/containerd/containerd.sock
 else
 CONTAINERD_SOCK:=/var/run/docker/containerd/docker-containerd.sock
