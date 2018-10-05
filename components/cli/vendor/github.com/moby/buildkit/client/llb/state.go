d60152ea514ddbd5bc30a646b475643559c76eb9
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2018-10-05 18:05:42 +0900

    bump up buildkit
    Signed-off-by: Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
    (cherry picked from commit 846c38cbd710c64fcbfd1876cfd90368bb46acde)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    Upstream-commit: 50f529fa4745961dfc7eba51a761445e67ca1a8d
    Component: cli

--- components/cli/vendor/github.com/moby/buildkit/client/llb/state.go
+++ components/cli/vendor/github.com/moby/buildkit/client/llb/state.go
@@ -9,7 +9,6 @@ import (
 	"github.com/moby/buildkit/identity"
 	"github.com/moby/buildkit/solver/pb"
 	"github.com/moby/buildkit/util/apicaps"
-	"github.com/moby/buildkit/util/system"
 	digest "github.com/opencontainers/go-digest"
 	specs "github.com/opencontainers/image-spec/specs-go/v1"
 )
@@ -34,7 +33,6 @@ func NewState(o Output) State {
 		ctx: context.Background(),
 	}
 	s = dir("/")(s)
-	s = addEnv("PATH", system.DefaultPathEnv)(s)
 	s = s.ensurePlatform()
 	return s
 }
@@ -411,6 +409,13 @@ func WithoutDefaultExportCache() ConstraintsOpt {
 	})
 }
 
+// WithCaps exposes supported LLB caps to the marshaler
+func WithCaps(caps apicaps.CapSet) ConstraintsOpt {
+	return constraintsOptFunc(func(c *Constraints) {
+		c.Caps = &caps
+	})
+}
+
 type constraintsWrapper struct {
 	Constraints
 }
@@ -424,6 +429,7 @@ type Constraints struct {
 	WorkerConstraints []string
 	Metadata          pb.OpMetadata
 	LocalUniqueID     string
+	Caps              *apicaps.CapSet
 }
 
 func Platform(p specs.Platform) ConstraintsOpt {
