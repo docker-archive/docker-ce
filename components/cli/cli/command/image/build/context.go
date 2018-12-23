18ea55cdbc9bf3db3abb7e11a3d8ff385c8c66a2
Nao YONASHIRO <owan.orisano@gmail.com>
2018-12-23 21:05:23 +0900

    test: add filepathMatches test
    Signed-off-by: Nao YONASHIRO <owan.orisano@gmail.com>
    Upstream-commit: 446762dc19073a941dc48adbbf09c80c2c696d4e
    Component: cli

--- components/cli/cli/command/image/build/context.go
+++ components/cli/cli/command/image/build/context.go
@@ -46,14 +46,6 @@ func ValidateContextDirectory(srcPath string, excludes []string) error {
 	if err != nil {
 		return err
 	}
-	matches := func(file string) (bool, error) {
-		file = filepath.Clean(file)
-		if file == "." {
-			// Don't let them exclude everything, kind of silly.
-			return false, nil
-		}
-		return pm.Matches(file)
-	}
 
 	return filepath.Walk(contextRoot, func(filePath string, f os.FileInfo, err error) error {
 		if err != nil {
@@ -69,7 +61,7 @@ func ValidateContextDirectory(srcPath string, excludes []string) error {
 		// skip this directory/file if it's not in the path, it won't get added to the context
 		if relFilePath, err := filepath.Rel(contextRoot, filePath); err != nil {
 			return err
-		} else if skip, err := matches(relFilePath); err != nil {
+		} else if skip, err := filepathMatches(pm, relFilePath); err != nil {
 			return err
 		} else if skip {
 			if f.IsDir() {
@@ -93,6 +85,15 @@ func ValidateContextDirectory(srcPath string, excludes []string) error {
 		}
 		return nil
 	})
+}
+
+func filepathMatches(matcher *fileutils.PatternMatcher, file string) (bool, error) {
+	file = filepath.Clean(file)
+	if file == "." {
+		// Don't let them exclude everything, kind of silly.
+		return false, nil
+	}
+	return matcher.Matches(file)
 }
 
 // DetectArchiveReader detects whether the input stream is an archive or a
