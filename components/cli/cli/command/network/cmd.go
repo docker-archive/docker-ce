1bd388e2aa03e6aa00a1cbc0218df781ed383e0a
Sebastiaan van Stijn <github@gone.nl>
2019-01-30 01:06:09 +0100

    Hide "builder" and "network" commands on old API versions
    - The `/build/prune` endpoint was added in API v1.31
    - The `/network` endpoints were added in API v1.21
    
    This patch hides these commands on older API versions
    
    Before this change:
    
    ```
    DOCKER_API_VERSION=1.0 docker
    
    ...
    
    Management Commands:
      builder     Manage builds
      container   Manage containers
      image       Manage images
      manifest    Manage Docker image manifests and manifest lists
      network     Manage networks
      system      Manage Docker
      trust       Manage trust on Docker images
    ```
    
    After this change
    
    ```
    DOCKER_API_VERSION=1.0 docker
    
    ...
    
    Management Commands:
      container   Manage containers
      image       Manage images
      manifest    Manage Docker image manifests and manifest lists
      system      Manage Docker
      trust       Manage trust on Docker images
    ```
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    Upstream-commit: 99fb2c1baa2b8edd87ae10b1fc6f65aebe7a18e7
    Component: cli

--- components/cli/cli/command/network/cmd.go
+++ components/cli/cli/command/network/cmd.go
@@ -10,10 +10,11 @@ import (
 // NewNetworkCommand returns a cobra command for `network` subcommands
 func NewNetworkCommand(dockerCli command.Cli) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "network",
-		Short: "Manage networks",
-		Args:  cli.NoArgs,
-		RunE:  command.ShowHelp(dockerCli.Err()),
+		Use:         "network",
+		Short:       "Manage networks",
+		Args:        cli.NoArgs,
+		RunE:        command.ShowHelp(dockerCli.Err()),
+		Annotations: map[string]string{"version": "1.21"},
 	}
 	cmd.AddCommand(
 		newConnectCommand(dockerCli),
