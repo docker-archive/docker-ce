b5349a9277c2ad35e85d52af9f54b8a91d6cac07
Ian Campbell <ijc@docker.com>
2019-03-06 12:07:04 +0000

    dial-stdio: handle connections which lack CloseRead method.
    This happens on Windows when dialing a named pipe (a path which is used by CLI
    plugins), in that case some debugging shows:
    
        DEBU[0000] conn is a *winio.win32MessageBytePipe
        DEBU[0000] conn is a halfReadCloser: false
        DEBU[0000] conn is a halfWriteCloser: true
        the raw stream connection does not implement halfCloser
    In such cases we can simply wrap with a nop function since closing for read
    isn't too critical.
    
    Signed-off-by: Ian Campbell <ijc@docker.com>
    Upstream-commit: 8919bbf04d3222ed1e3135f6b9e9bb126f40998d
    Component: cli

--- components/cli/cli/command/system/dial_stdio.go
+++ components/cli/cli/command/system/dial_stdio.go
@@ -34,10 +34,17 @@ func runDialStdio(dockerCli command.Cli) error {
 	if err != nil {
 		return errors.Wrap(err, "failed to open the raw stream connection")
 	}
-	connHalfCloser, ok := conn.(halfCloser)
-	if !ok {
+
+	var connHalfCloser halfCloser
+	switch t := conn.(type) {
+	case halfCloser:
+		connHalfCloser = t
+	case halfReadWriteCloser:
+		connHalfCloser = &nopCloseReader{t}
+	default:
 		return errors.New("the raw stream connection does not implement halfCloser")
 	}
+
 	stdin2conn := make(chan error)
 	conn2stdout := make(chan error)
 	go func() {
@@ -88,6 +95,19 @@ type halfWriteCloser interface {
 type halfCloser interface {
 	halfReadCloser
 	halfWriteCloser
+}
+
+type halfReadWriteCloser interface {
+	io.Reader
+	halfWriteCloser
+}
+
+type nopCloseReader struct {
+	halfReadWriteCloser
+}
+
+func (x *nopCloseReader) CloseRead() error {
+	return nil
 }
 
 type halfReadCloserWrapper struct {
