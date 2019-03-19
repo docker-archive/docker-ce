a7d3b89cc28482568a12cd982021b165acb93693
Stefan Scherer <stefan.scherer@docker.com>
2019-03-19 09:30:14 -0700

    Search Windows CLI plugins also in ProgramFiles
    Signed-off-by: Stefan Scherer <stefan.scherer@docker.com>
    Upstream-commit: 4d3a76d71e368b7ccff6184d2c65e588b543f293
    Component: cli

--- components/cli/cli-plugins/manager/manager_windows.go
+++ components/cli/cli-plugins/manager/manager_windows.go
@@ -7,4 +7,5 @@ import (
 
 var defaultSystemPluginDirs = []string{
 	filepath.Join(os.Getenv("ProgramData"), "Docker", "cli-plugins"),
+	filepath.Join(os.Getenv("ProgramFiles"), "Docker", "cli-plugins"),
 }
