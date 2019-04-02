949cd3a5a57dd656efe2ba1ffeeb2c42c9e6ad46
Silvin Lubecki <silvin.lubecki@docker.com>
2019-04-02 13:22:30 +0200

    cli/command/image/build_session.go:133:45: getBuildSharedKey - result 1 (error) is always nil (unparam)
    Signed-off-by: Silvin Lubecki <silvin.lubecki@docker.com>
    (cherry picked from commit 75c60c1af7c8a3ca79e1af2f351b5b0d29960900)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    Upstream-commit: a3912a4713db1d64d33e56df560435e6a99a6d41
    Component: cli

--- components/cli/cli/command/image/build_session.go
+++ components/cli/cli/command/image/build_session.go
@@ -35,16 +35,13 @@ func isSessionSupported(dockerCli command.Cli, forStream bool) bool {
 }
 
 func trySession(dockerCli command.Cli, contextDir string, forStream bool) (*session.Session, error) {
-	var s *session.Session
-	if isSessionSupported(dockerCli, forStream) {
-		sharedKey, err := getBuildSharedKey(contextDir)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to get build shared key")
-		}
-		s, err = session.NewSession(context.Background(), filepath.Base(contextDir), sharedKey)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to create session")
-		}
+	if !isSessionSupported(dockerCli, forStream) {
+		return nil, nil
+	}
+	sharedKey := getBuildSharedKey(contextDir)
+	s, err := session.NewSession(context.Background(), filepath.Base(contextDir), sharedKey)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create session")
 	}
 	return s, nil
 }
@@ -130,10 +127,10 @@ func (bw *bufferedWriter) String() string {
 	return fmt.Sprintf("%s", bw.Writer)
 }
 
-func getBuildSharedKey(dir string) (string, error) {
+func getBuildSharedKey(dir string) string {
 	// build session is hash of build dir with node based randomness
 	s := sha256.Sum256([]byte(fmt.Sprintf("%s:%s", tryNodeIdentifier(), dir)))
-	return hex.EncodeToString(s[:]), nil
+	return hex.EncodeToString(s[:])
 }
 
 func tryNodeIdentifier() string {
