tiborvass/docker

Integration: exclude IPVlan test-suite on Windows
Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

integration/network/ipvlan/main_windows_test.go
@@ -1,0 +1,1 @@
+package ipvlan // import "github.com/tiborvass/docker/integration/network/ipvlan"

docker-archive/docker-ce

(cherry picked from commit 4060a7026c0ffa2983ab1fa41d43da4937ef63c6)
Upstream-commit: 1710bba5c337aa3d997c7fa3b64ebf2b971ffb8e
Component: engine

components/engine/integration/network/ipvlan/main_windows_test.go
@@ -1,0 +1,1 @@
+package ipvlan // import "github.com/docker/docker/integration/network/ipvlan"