tiborvass/docker

prepare for eg on waitAndAssert
Signed-off-by: Tibor Vass <tibor@docker.com>

template.waitAndAssert.go
@@ -1,0 +1,40 @@
+// +build ignore
+
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/tiborvass/docker/integration-cli/checker"
+	"gotest.tools/assert"
+	"gotest.tools/poll"
+)
+
+func pollCheck(t *testing.T, f interface{}, compare func(x interface{}) assert.BoolOrComparison) poll.Check
+
+type eg_compareFunc func(...interface{}) checker.Compare
+
+type waitAndAssertFunc func(t *testing.T, timeout time.Duration, ff, comparison interface{}, args ...interface{})
+
+func before(
+	waitAndAssert waitAndAssertFunc,
+	t *testing.T,
+	timeout time.Duration,
+	f interface{},
+	comparison interface{},
+	args ...interface{}) {
+
+	waitAndAssert(t, timeout, f, comparison, args...)
+}
+
+func after(
+	waitAndAssert waitAndAssertFunc,
+	t *testing.T,
+	timeout time.Duration,
+	f interface{},
+	comparison interface{},
+	args ...interface{}) {
+
+	poll.WaitOn(t, pollCheck(t, f, comparison.(eg_compareFunc)(args...)), poll.WithTimeout(timeout))
+}

docker-archive/docker-ce

(cherry picked from commit 42599f1cad9f5eaa3f3cca5a9de43b7ff2c00006)
Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
Upstream-commit: d766dac3bf4ea7b185c068cf6b308d995972701f
Component: engine

components/engine/template.waitAndAssert.go
@@ -1,0 +1,40 @@
+// +build ignore
+
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/docker/docker/integration-cli/checker"
+	"gotest.tools/assert"
+	"gotest.tools/poll"
+)
+
+func pollCheck(t *testing.T, f interface{}, compare func(x interface{}) assert.BoolOrComparison) poll.Check
+
+type eg_compareFunc func(...interface{}) checker.Compare
+
+type waitAndAssertFunc func(t *testing.T, timeout time.Duration, ff, comparison interface{}, args ...interface{})
+
+func before(
+	waitAndAssert waitAndAssertFunc,
+	t *testing.T,
+	timeout time.Duration,
+	f interface{},
+	comparison interface{},
+	args ...interface{}) {
+
+	waitAndAssert(t, timeout, f, comparison, args...)
+}
+
+func after(
+	waitAndAssert waitAndAssertFunc,
+	t *testing.T,
+	timeout time.Duration,
+	f interface{},
+	comparison interface{},
+	args ...interface{}) {
+
+	poll.WaitOn(t, pollCheck(t, f, comparison.(eg_compareFunc)(args...)), poll.WithTimeout(timeout))
+}