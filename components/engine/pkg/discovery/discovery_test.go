tiborvass/docker

rm-gocheck: run goimports to compile successfully
goimports -w \
-- "./integration-cli/daemon" "./pkg/discovery" "./pkg/discovery/file" "./pkg/discovery/kv" "./pkg/discovery/memory" "./pkg/discovery/nodes" "./integration-cli" \
&& \
 gofmt -w -s \
-- "./integration-cli/daemon" "./pkg/discovery" "./pkg/discovery/file" "./pkg/discovery/kv" "./pkg/discovery/memory" "./pkg/discovery/nodes" "./integration-cli"

Signed-off-by: Tibor Vass <tibor@docker.com>

pkg/discovery/discovery_test.go
@@ -3,7 +3,8 @@ package discovery // import "github.com/tiborvass/docker/pkg/discovery"
 import (
 	"testing"
 
-	"github.com/go-check/check"
+	"github.com/tiborvass/docker/integration-cli/checker"
+	"gotest.tools/assert"
 )
 
 // Hook up gocheck into the "go test" runner.
@@ -11,7 +12,7 @@ func Test(t *testing.T) { /*check.TestingT(t)*/ }
 
 type DiscoverySuite struct{}
 
- /*check.Suite(&DiscoverySuite{})*/
+/*check.Suite(&DiscoverySuite{})*/
 
 func (s *DiscoverySuite) TestNewEntry(c *testing.T) {
 	entry, err := NewEntry("127.0.0.1:2375")

docker-archive/docker-ce

(cherry picked from commit 59e55dcdd063d3131ed1992254350b7014d1bf1f)
Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
Upstream-commit: e25352a42ac1874027e502574ba69bb25ae6ef62
Component: engine

components/engine/pkg/discovery/discovery_test.go
@@ -3,7 +3,8 @@ package discovery // import "github.com/docker/docker/pkg/discovery"
 import (
 	"testing"
 
-	"github.com/go-check/check"
+	"github.com/docker/docker/integration-cli/checker"
+	"gotest.tools/assert"
 )
 
 // Hook up gocheck into the "go test" runner.
@@ -11,7 +12,7 @@ func Test(t *testing.T) { /*check.TestingT(t)*/ }
 
 type DiscoverySuite struct{}
 
- /*check.Suite(&DiscoverySuite{})*/
+/*check.Suite(&DiscoverySuite{})*/
 
 func (s *DiscoverySuite) TestNewEntry(c *testing.T) {
 	entry, err := NewEntry("127.0.0.1:2375")