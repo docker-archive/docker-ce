feabf9a1a7c2a597c13629721e542ca476e82dcd
Sebastiaan van Stijn <github@gone.nl>
2019-10-29 01:44:16 +0100

    e2e/container: containerExistsWithStatus - t is unused (unparam)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit 6205ef33be7fb2afa6ce6f1ea163f225ddca1434)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    Upstream-commit: 7467a6a7de6a93bcc232bf19e193d3ecdd96fb8d
    Component: cli

--- components/cli/e2e/container/proxy_signal_test.go
+++ components/cli/e2e/container/proxy_signal_test.go
@@ -26,17 +26,17 @@ func TestSigProxyWithTTY(t *testing.T) {
 	assert.NilError(t, err, "failed to start container")
 	defer icmd.RunCommand("docker", "container", "rm", "-f", t.Name())
 
-	poll.WaitOn(t, containerExistsWithStatus(t, t.Name(), "running"), poll.WithDelay(100*time.Millisecond), poll.WithTimeout(5*time.Second))
+	poll.WaitOn(t, containerExistsWithStatus(t.Name(), "running"), poll.WithDelay(100*time.Millisecond), poll.WithTimeout(5*time.Second))
 
 	pid := cmd.Process.Pid
 	t.Logf("terminating PID %d", pid)
 	err = syscall.Kill(pid, syscall.SIGTERM)
 	assert.NilError(t, err)
 
-	poll.WaitOn(t, containerExistsWithStatus(t, t.Name(), "exited"), poll.WithDelay(100*time.Millisecond), poll.WithTimeout(5*time.Second))
+	poll.WaitOn(t, containerExistsWithStatus(t.Name(), "exited"), poll.WithDelay(100*time.Millisecond), poll.WithTimeout(5*time.Second))
 }
 
-func containerExistsWithStatus(t *testing.T, containerID, status string) func(poll.LogT) poll.Result {
+func containerExistsWithStatus(containerID, status string) func(poll.LogT) poll.Result {
 	return func(poll.LogT) poll.Result {
 		result := icmd.RunCommand("docker", "inspect", "-f", "{{ .State.Status }}", containerID)
 		// ignore initial failures as the container may not yet exist (i.e., don't result.Assert(t, icmd.Success))
