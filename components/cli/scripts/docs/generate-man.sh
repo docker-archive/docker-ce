8a39c8464b9b9f76abe9a366a0dbc2702c6eef22
Arnaud Rebillout <elboulangero@gmail.com>
2020-12-16 10:19:43 +0700

    Rename bin/md2man to bin/go-md2man
    In the recent PR !2877, some code was added to check if md2man is
    already installed in the build environment. This is to cater to the
    needs of Linux distributions.
    
    However it turns out that Linux distributions install md2man as
    bin/go-md2man instead of bin/md2man, hence the PR !2877 doesn't help
    much.
    
    This commit fixes it by settling on using the binary name go-md2man.
    
    For reference, here the file list of the package go-md2man in several
    distributions:
    
    - Debian: <https://packages.debian.org/sid/amd64/go-md2man/filelist>
    - Ubuntu: <https://packages.ubuntu.com/hirsute/amd64/go-md2man/filelist>
    - Fedora: <https://fedora.pkgs.org/31/fedora-x86_64/golang-github-cpuguy83-md2man-2.0.0-0.4.20190624gitf79a8a8.fc31.x86_64.rpm.html>
    - ArchLinux: <https://www.archlinux.org/packages/community/x86_64/go-md2man/>
    
    Signed-off-by: Arnaud Rebillout <elboulangero@gmail.com>
    Upstream-commit: 6e2607c6a68ecf1a7378133f22cb7192e2eb9d5b
    Component: cli

--- components/cli/scripts/docs/generate-man.sh
+++ components/cli/scripts/docs/generate-man.sh
@@ -4,9 +4,9 @@ set -eu -o pipefail
 
 mkdir -p ./man/man1
 
-if ! command -v md2man &> /dev/null; then
+if ! command -v go-md2man &> /dev/null; then
 	# yay, go install creates a binary named "v2" ¯\_(ツ)_/¯
-	go build -o "/go/bin/md2man" ./vendor/github.com/cpuguy83/go-md2man/v2
+	go build -o "/go/bin/go-md2man" ./vendor/github.com/cpuguy83/go-md2man/v2
 fi
 
 # Generate man pages from cobra commands
