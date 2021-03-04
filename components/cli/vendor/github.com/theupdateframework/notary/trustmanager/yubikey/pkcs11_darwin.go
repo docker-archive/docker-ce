4d47b5f2f8f58a31978b3013780bfd874511590e
Tonis Tiigi <tonistiigi@gmail.com>
2021-03-03 23:49:48 -0800

    vendor: update notary to 5f1f4a34
    Brings in fixes for darwin/arm64 targets
    
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>
    Upstream-commit: a54577b7576b6d32d1abf00c8ff70950b7a065f5
    Component: cli

--- components/cli/vendor/github.com/theupdateframework/notary/trustmanager/yubikey/pkcs11_darwin.go
+++ components/cli/vendor/github.com/theupdateframework/notary/trustmanager/yubikey/pkcs11_darwin.go
@@ -6,4 +6,6 @@ var possiblePkcs11Libs = []string{
 	"/usr/local/lib/libykcs11.dylib",
 	"/usr/local/docker/lib/libykcs11.dylib",
 	"/usr/local/docker-experimental/lib/libykcs11.dylib",
+	// default location on arm64
+	"/opt/homebrew/lib/libykcs11.dylib",
 }
